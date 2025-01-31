791c1a3a58040a3d0f8036f24062d0e6110d8e19
Kevin <kev.enrri@hotmail.com>
2025-01-31 12:47:36 -0500

    feat: Login rediretion

--- app/src/main/java/com/xperiencelabs/arapp/FormLoginActivity.kt
+++ app/src/main/java/com/xperiencelabs/arapp/FormLoginActivity.kt
@@ -1,13 +1,48 @@
 package com.xperiencelabs.arapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class FormLoginActivity: AppCompatActivity () {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
+        auth = Firebase.auth
+        val emailField = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val passwordField = findViewById<EditText>(R.id.editTextTextPassword)
+        val loginButton = findViewById<Button>(R.id.tryToLogin)
+
+        loginButton.setOnClickListener {
+            val email = emailField.text.toString()
+            val password = passwordField.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signIn(email, password)
+            } else {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user =  auth.currentUser
+                    setContentView(R.layout.activity_main)
+                } else {
+                    Toast.makeText(
+                        baseContext,
+                        "Auth Failed",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
     }
 }
